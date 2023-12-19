3d8f8c7d1829fff62c7d352f3ef183ffcf944d7e
Spectre212 <eassam131@gmail.com>
2023-12-19 03:38:03 -0800

    classes

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+class Square:
+    """Represents a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: Length of the side of the square.
+        """
+        self.__size = size
