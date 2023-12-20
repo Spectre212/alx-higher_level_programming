0a3d41687a1b4139031feef6de543c61f5cf72b8
Spectre212 <eassam131@gmail.com>
2023-12-19 19:20:49 -0800

    import_modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
 """    sum_result = add(a, b)
     diff_result = sub(a, b)
     product_result = mul(a, b)"""
-        print("{} + {} = {}".format(a, b, add(a,b)))
-        print("{} - {} = {}".format(a, b, sub(a,b)))
-        print("{} * {} = {}".format(a, b, mul(a,b)))
-        print("{} / {} = {}".format(a, b, div(a,b)))
+        
+    print("{} + {} = {}".format(a, b, add(a,b)))
+    print("{} - {} = {}".format(a, b, sub(a,b)))
+    print("{} * {} = {}".format(a, b, mul(a,b)))
+    print("{} / {} = {}".format(a, b, div(a,b)))
