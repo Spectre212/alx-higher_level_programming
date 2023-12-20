a59ad8518e716a1af3457aa9d896af45790fd373
Spectre212 <eassam131@gmail.com>
2023-12-19 18:37:29 -0800

    import_modules

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a, b):
+    """Addition Function
+
+    Args:
+        a (int): First integer.
+        b (int): Second integer.
+
+    Returns:
+        int: The result of the addition, a + b.
+    """
+    return a + b
